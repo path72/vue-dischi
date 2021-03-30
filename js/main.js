5fe5cf770aaf99e2e883fd82f3ef23eb9de8d08e
Salvatore Trimarchi <path72@gmail.com>
2021-03-30 20:22:31 +0200

    bonus1: la simmetria è vita

--- js/main.js
+++ js/main.js
@@ -6,18 +6,52 @@ var app = new Vue(
 	{
 		el: '#app',
 		data: {
+			// ** DATA IN ** //
 			itemList: [],
-			itemListIsEmpty: true
+			itemListIsEmpty: true,
+			// ** DATA OUT ** //
+			filterLists: {}, 		// oggetto di parametri 1 con liste di parametri 2
+			filters: [], 			// lista di parametri 1
+			filter1Selected: '', 	// parametro 1 selezionato
+			filter2Selected: ''		// parametro 2 selezionato
 		},
 		methods: {
 			getRemoteData() {
 				axios
-					.get(' https://flynn.boolean.careers/exercises/api/array/music')
+					.get('https://flynn.boolean.careers/exercises/api/array/music')
 					.then((resp)=>{
-						this.itemList = resp.data.response;
+						this.itemList = resp.data.response; // array = array
 						if (this.itemList.length > 0) this.itemListIsEmpty = false;
-						console.log(this.itemList);
+						this.buildFilterList(this.itemList);
 					});
+				},
+			buildFilterList(items) {
+				items.forEach((el)=>{
+					for (key in el) {
+						if (this.filterLists[key] == undefined) {
+							this.filterLists[key] = [];
+							this.filters.push(key);
+						}
+						if (!this.filterLists[key].includes(el[key])) this.filterLists[key].push(el[key]);
+					}
+				});
+				console.log(this.filterLists);
+				console.log(this.filters);
+			},
+			filter1Selection() {
+				console.log(this.filter1Selected);
+				this.filter2Selected = '';
+			},
+			filter2Selection() {
+				console.log(this.filter2Selected);
+			},
+			isViewable(item) {
+				if (this.filter1Selected && this.filter2Selected)
+					return (item[this.filter1Selected] == this.filter2Selected);
+				else return true;
+			},
+			cap(string) {
+				return string[0].toUpperCase()+string.substring(1);
 			}
 		},
 		computed: {
